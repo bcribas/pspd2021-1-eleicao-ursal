6bbfd496727b26d66e39ce5fd92a53c40c0a1c63
Danillo Souza <danillosouza1704@gmail.com>
2021-09-29 19:06:29 -0300

    Adicionado atomic para corrigir saida

--- relatorios/ana_danillo_esio_pedro/leitura_1.c
+++ relatorios/ana_danillo_esio_pedro/leitura_1.c
@@ -1,8 +1,8 @@
+#include <math.h>
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
 #include <string.h>
-#include <math.h>
 
 #define true 1
 #define false 0
@@ -16,20 +16,19 @@ void printElected(int *politicianArray, int maxElected, int minSize, int maxSize
   int *best = calloc(maxElected, sizeof(int));
   int secondRound = false;
   for (int i = 0; i < maxElected; i++) {
-      int elected = 0;
-      for (int j = minSize; j < maxSize; j++) {
-          if (politicianArray[j] > politicianArray[elected]) {
-              elected = j;
-              secondRound = false;
-          }
-          else if (politicianArray[j] == politicianArray[elected]) {
-              if (minSize != MIN_P)
-                  elected = j > elected ? j : elected;
-              secondRound = true;
-          }
+    int elected = 0;
+    for (int j = minSize; j < maxSize; j++) {
+      if (politicianArray[j] > politicianArray[elected]) {
+        elected = j;
+        secondRound = false;
+      } else if (politicianArray[j] == politicianArray[elected]) {
+        if (minSize != MIN_P)
+          elected = j > elected ? j : elected;
+        secondRound = true;
       }
-      best[i] = elected;
-      politicianArray[elected] = -1;
+    }
+    best[i] = elected;
+    politicianArray[elected] = -1;
   }
 
   if (minSize == MIN_P) {
@@ -73,24 +72,25 @@ int main(int argc, char *argv[]) {
 
   long int size = end_position - start_position;
 
-  int* thread_starts = calloc(num_threads + 1, sizeof(long int));
+  int *thread_starts = calloc(num_threads + 1, sizeof(long int));
   thread_starts[0] = start_position;
   thread_starts[num_threads] = end_position;
 
   int chunk_size = (int)floor(size / num_threads);
 
-  for(int i = 1; i < num_threads; i++) {
-    fseek(file, thread_starts[i-1] + chunk_size, SEEK_SET);
+  for (int i = 1; i < num_threads; i++) {
+    fseek(file, thread_starts[i - 1] + chunk_size, SEEK_SET);
     char line[10];
     fgets(line, 10, file);
-    thread_starts[i] = thread_starts[i-1] + chunk_size + strlen(line);
+    thread_starts[i] = thread_starts[i - 1] + chunk_size + strlen(line);
   }
 
   fclose(file);
 
-  #pragma omp parallel reduction(+ : validVote, invalidVote, totalVotesPresident, president, senator, congressman, congressperson)
+#pragma omp parallel reduction(+ \
+                               : validVote, invalidVote, totalVotesPresident)
   {
-    FILE* file = fopen(argv[1], "r");
+    FILE *file = fopen(argv[1], "r");
     long int start, end;
 
     start = thread_starts[omp_get_thread_num()];
@@ -102,7 +102,7 @@ int main(int argc, char *argv[]) {
 
     long int counter = start;
 
-    while(fscanf(file, "%d%n", &vote, &size) != EOF) {
+    while (fscanf(file, "%d%n", &vote, &size) != EOF) {
       counter += size;
       if (counter >= end) {
         break;
@@ -111,25 +111,27 @@ int main(int argc, char *argv[]) {
         validVote++;
         if (vote < MAX_P) {
           totalVotesPresident++;
+#pragma omp atomic
           president[vote]++;
         } else if (vote < MAX_S) {
+#pragma omp atomic
           senator[vote]++;
         } else if (vote < MAX_CM) {
+#pragma omp atomic
           congressman[vote]++;
         } else {
+#pragma omp atomic
           congressperson[vote]++;
         }
-      } else 
+      } else
         invalidVote++;
     }
   }
 
   printf("%d %d\n", validVote, invalidVote);
-
   printElected(president, 1, MIN_P, MAX_P, totalVotesPresident);
   printElected(senator, totalSenators, MAX_P, MAX_S, false);
   printElected(congressman, totalCongressman, MAX_S, MAX_CM, false);
   printElected(congressperson, totalCongressperson, MAX_CM, MAX_CP, false);
-
   return 0;
 }
