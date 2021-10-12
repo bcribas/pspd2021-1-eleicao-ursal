1da8211c1d999ceb28fe32cf0ab9e2f66b2b758b
Pedro <pedroigor.silva@outlook.com>
2021-10-12 15:41:45 -0300

    ake all reads inside parallel

--- relatorios/Marcos-Pedro-Rogerio-Youssef/src/main.c
+++ relatorios/Marcos-Pedro-Rogerio-Youssef/src/main.c
@@ -4,6 +4,9 @@
 #include <math.h>
 #include <string.h>
 #define MAX_ARRAY_SIZE 100000
+#ifndef THREADS
+    #define THREADS 1
+#endif
 int encontrar_candidato(int *votos, long int size)
 {
     unsigned int maior = 0;
@@ -32,82 +35,56 @@ float porcentagem_presidente(int *votos, int total)
     }
     return (float)maior / total;
 }
-long int votes_start_position;
-int chunk_size;
-int threads;
-int get_start_position(int thread, FILE * file){
+int get_start_position(int thread, FILE * file, int votes_start_position, int chunk_size, int threads){
     long int position = votes_start_position;
     for(int i = 0; i < thread; i++){
         position+=chunk_size;
     }
-    if(thread>0){
+    if(thread>0 && thread < threads){
         char line[7];
         fseek(file, position, SEEK_SET);
         fgets(line, 10, file);
         position+=strlen(line);
     }
-    //printf("start: %d\n", position);
     return position;
 }
-int get_end_position(int thread, FILE * file){
-    return get_start_position(thread+1, file);
+int get_end_position(int thread, FILE * file, int votes_start_position, int chunk_size, int threads){
+    return get_start_position(thread+1, file, votes_start_position, chunk_size, threads);
 }
 int main(int argc, char *argv[])
 {
+    omp_set_num_threads(THREADS);
     const char *in = argv[1];
-    threads = atoi(argv[2]);
-    FILE *file = fopen(in, "r");
-
-    long int s, f, e;
     const int size_p = 100, size_s = 1000, size_f = 10000, size_e = 100000;
 
-    fscanf(file, "%ld %ld %ld\n", &s, &f, &e);
     int **matrix = calloc(16, 1 + sizeof(int *));
-    int *max = calloc(16, sizeof(int));
     for (int i = 0; i < 16; i++)
     {
         matrix[i] = calloc(MAX_ARRAY_SIZE, sizeof(int));
     }
     int *votos = calloc(MAX_ARRAY_SIZE, sizeof(int));
 
-    votes_start_position = ftell(file);
-    fseek(file, 0L, SEEK_END);
-    long int votes_end_position = ftell(file);
-    fseek(file, votes_start_position, SEEK_SET);
-    long int size = votes_end_position - votes_start_position;
-    //printf("end: %d\n", votes_end_position);
-    //int *threads_read_positions = calloc(threads + 1, sizeof(int));
-    //threads_read_positions[threads] = votes_end_position - 1;
-    chunk_size = (int)floor(size / threads);
-    int thread = 0;
-    /*while (thread < threads)
-    {
-        char line[1000];
-        if (thread == 0)
-        {
-            threads_read_positions[thread] = votes_start_position;
-        }
-        else
-        {
-            fseek(file, threads_read_positions[thread - 1] + chunk_size, SEEK_SET);
-            fgets(line, 10, file);
-            threads_read_positions[thread] = threads_read_positions[thread - 1] + chunk_size + strlen(line);
-            printf("old: %d\n", threads_read_positions[thread]);
-        }
-        thread++;
-    }*/
-    fclose(file);
+    int threads;
     int quantidade_p = 0;
     long int validos = 0, invalidos = 0;
     char *chunk_file[16];
-#pragma omp parallel num_threads(threads) reduction(+ \
+    long int s, f, e;
+#pragma omp parallel reduction(+ \
                                : validos, invalidos, quantidade_p)
     {
+        
         FILE *read_file = fopen(in, "r");
+        fscanf(read_file, "%ld %ld %ld\n", &s, &f, &e);
+        int votes_start_position = ftell(read_file);
+        fseek(read_file, 0L, SEEK_END);
+        long int votes_end_position = ftell(read_file);
+        long int bytes_size = votes_end_position - votes_start_position;
+        threads = omp_get_num_threads();
+        int chunk_size = (int)floor(bytes_size / threads);
         int voto;
         int thread_num = omp_get_thread_num();
-        int start_position = get_start_position(thread_num, read_file);
-        int end_position = get_end_position(thread_num, read_file);
+        int start_position = get_start_position(thread_num, read_file, votes_start_position, chunk_size, threads);
+        int end_position = get_end_position(thread_num, read_file, votes_start_position, chunk_size, threads);
         
         fseek(read_file, start_position, SEEK_SET);
         char *line;
@@ -129,7 +106,6 @@ int main(int argc, char *argv[])
                 {
                     quantidade_p++;
                 }
-                //=max[thread_num] = voto > max[thread_num]? voto:max[thread_num];
                 #pragma omp atomic
                 matrix[thread_num][voto]++;
             }
@@ -177,7 +153,7 @@ int main(int argc, char *argv[])
         printf("%d ", encontrar_candidato(votos, size_e));
     }
     printf("%d\n", encontrar_candidato(votos, size_e));
+    free(matrix);
     free(votos);
-    free(max);
     return 0;
 }
