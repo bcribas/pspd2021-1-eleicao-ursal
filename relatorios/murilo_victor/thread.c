a4dc5963c2f4f544c0db9cbc59ddf5290e742c31
murilo-dan <murilo.loiola.dan@gmail.com>
2021-11-03 16:53:16 -0300

    Retirando vetores desencessários, organizando variáveis e limpando main.
    Co-authored-by: Victor Amaral <victoramaralce@gmail.com>

--- relatorios/murilo_victor/thread.c
+++ relatorios/murilo_victor/thread.c
@@ -24,40 +24,31 @@ typedef int item;
             exch(A, B); \
     }
 
-Candidato results[17][100000] = {0, 0};
+Candidato results[16][100000] = {0, 0};
 
 // Funcoes
 void ordena(Candidato *v, int l, int r);
 void merge(Candidato *v, int l, int r1, int r2);
+void printResult(Candidato *final, double maioriaPresidente, int nSenadores, int nDepDeferal, int nDepEsadual);
 
 int main(int argc, char **argv)
 {
-    FILE *inputFile;
-
-    clock_t start, end;
-    double cpu_time_used;
-
-    int nSenadores, nDepFederal, nDepEstadual;
-    int votoPresidente = 0, votoValido = 0, votoInvalido = 0, voto;
-
     // Especificar numero de threads
     int nThreads = 8;
     if (nThreads > omp_get_max_threads())
     {
         nThreads = omp_get_max_threads();
     }
-    int threadStartingPoints[nThreads];
 
+    int nSenadores, nDepFederal, nDepEstadual;
+    int votoPresidente = 0, votoValido = 0, votoInvalido = 0, voto;
+    double maioriaPresidente;
+
+    int threadStartingPoints[nThreads];
     int startingPosition, chunkSize;
     long int endPosition, fileSize;
 
-    double maioriaPresidente;
-
-    Candidato presidente[100] = {0, 0};
-    Candidato senador[1000] = {0, 0};
-    Candidato depFederal[10000] = {0, 0};
-    Candidato depEstadual[100000] = {0, 0};
-
+    FILE *inputFile;
     inputFile = fopen(argv[1], "r");
 
     // Calculando byte inicial, byte final, bytes a serem lidos e divisão do arquivo em blocos
@@ -136,77 +127,59 @@ int main(int argc, char **argv)
         }
     }
 
+    Candidato final[100000] = {0};
+
     /*Cada thread escreve os resultados lidos em uma linha da matriz results,
-    evitando concorrência de acesso. Aqui, os valores lidos são somados e atribuidos
-    a ultima linha da matriz, que passa a possuir o resultado final dos votos*/
+    evitando concorrencia de acesso. Aqui, os valores lidos sao somados e atribuidos
+    ao vetor final, que passa a possuir o resultado final dos votos*/
     for (int i = 0; i < 16; i++)
     {
         for (int j = 0; j < 100000; j++)
         {
             if (results[i][j].qtdVotos > 0)
             {
-                results[16][j].nCandidato = results[i][j].nCandidato;
-                results[16][j].qtdVotos += results[i][j].qtdVotos;
+                final[j].nCandidato = results[i][j].nCandidato;
+                final[j].qtdVotos += results[i][j].qtdVotos;
             }
         }
     }
 
-    // Copia os resultados para arrays especificos, preparando para a ordenacao
-    for (int i = 10; i < 100; i++)
-    {
-        presidente[i].nCandidato = results[16][i].nCandidato;
-        presidente[i].qtdVotos = results[16][i].qtdVotos;
-    }
-
-    for (int i = 100; i < 1000; i++)
-    {
-        senador[i].nCandidato = results[16][i].nCandidato;
-        senador[i].qtdVotos = results[16][i].qtdVotos;
-    }
-
-    for (int i = 1000; i < 10000; i++)
-    {
-        depFederal[i].nCandidato = results[16][i].nCandidato;
-        depFederal[i].qtdVotos = results[16][i].qtdVotos;
-    }
-
-    for (int i = 10000; i < 100000; i++)
-    {
-        depEstadual[i].nCandidato = results[16][i].nCandidato;
-        depEstadual[i].qtdVotos = results[16][i].qtdVotos;
-    }
-
     // Ordena em ordem decrescente de acordo com qtdVotos
-    ordena(presidente, 0, 99);
-    ordena(senador, 0, 999);
-    ordena(depFederal, 0, 9999);
-    ordena(depEstadual, 0, 99999);
+    ordena(final, 0, 99);
+    ordena(final, 100, 999);
+    ordena(final, 1000, 9999);
+    ordena(final, 10000, 99999);
 
     printf("%d %d\n", votoValido, votoInvalido);
 
     maioriaPresidente = votoPresidente * 0.51;
 
-    if (presidente[0].qtdVotos > maioriaPresidente)
-        printf("%d\n", presidente[0].nCandidato);
+    printResult(final, maioriaPresidente, nSenadores, nDepFederal, nDepEstadual);
+
+    return 0;
+}
+
+void printResult(Candidato *final, double maioriaPresidente, int nSenadores, int nDepFederal, int nDepEstadual)
+{
+    if (final[0].qtdVotos > maioriaPresidente)
+        printf("%d\n", final[0].nCandidato);
     else
         printf("Segundo turno\n");
 
-    printf("%d", senador[0].nCandidato);
-    for (int i = 1; i < nSenadores; i++)
-        printf(" %d", senador[i].nCandidato);
+    printf("%d", final[100].nCandidato);
+    for (int i = 101; i < 100 + nSenadores; i++)
+        printf(" %d", final[i].nCandidato);
     printf("\n");
 
-    printf("%d", depFederal[0].nCandidato);
-    for (int i = 1; i < nDepFederal; i++)
-        printf(" %d", depFederal[i].nCandidato);
+    printf("%d", final[1000].nCandidato);
+    for (int i = 1001; i < 1000 + nDepFederal; i++)
+        printf(" %d", final[i].nCandidato);
     printf("\n");
 
-    printf("%d", depEstadual[0].nCandidato);
-    for (int i = 1; i < nDepEstadual; i++)
-        printf(" %d", depEstadual[i].nCandidato);
+    printf("%d", final[10000].nCandidato);
+    for (int i = 10001; i < 10000 + nDepEstadual; i++)
+        printf(" %d", final[i].nCandidato);
     printf("\n");
-
-    return 0;
 }
 
 void ordena(Candidato *v, int l, int r)
